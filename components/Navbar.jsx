830487d1132e19c3ea2f84dfaeaba416139c720c
asiraj <adamsirajswe@gmail.com>
2022-08-28 23:32:22 +0800

    setup default starter

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between py-6">
+      <h1 className="text-white font-poppins">AS</h1>
+      <p className="text-white">Links</p>
+    </nav>
+  );
+};
+
+export default Navbar;
