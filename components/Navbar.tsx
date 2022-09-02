c738260801fb1c2ac8a4f445c23b81c0a1944d6b
asiraj <adamsirajswe@gmail.com>
2022-09-02 18:26:29 +0800

    converted project to TypeScript

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-between py-6">
-      <h1 className="text-white font-poppins">AS</h1>
+      <h1 className="text-white">AS</h1>
       <p className="text-white">Links</p>
     </nav>
   );
